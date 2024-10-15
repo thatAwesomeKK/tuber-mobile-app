fcfde1a79dfa798e84a7a632726f470585c33f31
thatAwesomeKK <2550492.kk@gmail.com>
2024-10-15 13:24:17 +0530

    Update socket.ts

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
-const url = "https://gateway.thatawesomekk.eu.org";
-// const url = "http://192.168.29.13:5005";
+// const url = "https://gateway.thatawesomekk.eu.org";
+const url = "http://192.168.29.13:5005";
 
 export const socket = io(url, {
   autoConnect: false,
